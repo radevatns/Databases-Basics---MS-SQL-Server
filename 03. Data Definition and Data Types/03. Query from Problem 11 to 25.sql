c550ecdc9afa8312529616b677a8bd6466686bfd
radevatns <klementt@abv.bg>
2017-01-27 21:06:06 +0200

    ex 24 not work yet

---
+++ 03. Data Definition and Data Types/03. Query from Problem 11 to 25.sql
@@ -0,0 +1,123 @@
+SELECT * FROM Employees
+--11. Find All Employees Without Manager
+SELECT [FirstName]
+		,[LastName]
+		FROM Employees
+	WHERE ManagerID IS NULL
+
+--12. Find All Employees with Salary More Than 50000
+SELECT FirstName
+	,LastName
+	,Salary
+	FROM Employees
+	WHERE Salary > 50000
+	ORDER BY Salary DESC
+
+--13. Find 5 Best Paid Employees
+SELECT TOP (5)[FirstName]
+	,[LastName]
+	FROM Employees
+	Order by Salary DESC
+
+--14. Find All Employees Except Marketing
+SELECT [FirstName]
+,[LastName]
+,[DepartmentID]
+FROM Employees
+WHERE DepartmentID != 4
+
+--15. Sort Employees Table
+SELECT * FROM Employees
+ORDER BY Salary DESC, FirstName, LastName DESC, MiddleName
+
+--16. Create View Employees with Salaries
+CREATE VIEW [V_EmployeesSalaries] AS 
+SELECT [FirstName]
+	,[LastName]
+	,[Salary]
+	FROM Employees
+SELECT * FROM [V_EmployeesSalaries]
+
+--17. Create View Employees with Job Titles
+CREATE VIEW V_EmployeeNameJobTitle 
+AS
+	SELECT FirstName +' '+ ISNULL(MiddleName,'')+  ' ' + [LastName] AS "Full Name"
+,[JobTitle]
+	FROM Employees
+
+	SELECT * FROM V_EmployeeNameJobTitle
+ 
+--18. Distinct Job Titles
+SELECT DISTINCT JobTitle
+FROM Employees
+
+--19. Find First 10 Started Projects
+SELECT * FROM Projects
+
+SELECT TOP(10) * FROM Projects
+ORDER BY StartDate, Name
+
+--20. Last 7 Hired Employees
+SELECT TOP(7) [FirstName]
+,[LastName]
+,[HireDate] 
+FROM Employees
+ORDER BY HireDate DESC
+
+--21. Increase Salaries
+SELECT Salary FROM Employees
+WHERE DepartmentID IN (1,2,4,11)
+
+UPDATE Employees
+SET Salary = (Salary*1.12)
+WHERE DepartmentID IN (1, 2, 4, 11)
+--WHERE DepartmentID = 1 OR DepartmentID = 2 OR DepartmentID = 4 OR DepartmentID = 11
+SELECT Salary FROM Employees
+WHERE DepartmentID IN (1,2,4,11)
+
+UPDATE Employees
+SET Salary = (Salary/1.12)
+WHERE DepartmentID IN (1, 2, 4, 11)
+
+SELECT Salary FROM Employees
+WHERE DepartmentID IN (1,2,4,11)
+
+SELECT Salary FROM Employees
+
+
+
+--22. All Mountain Peaks
+USE Geography
+SELECT * FROM Peaks
+SELECT  [PeakName] FROM Peaks
+ORDER BY PeakName
+
+--23. Biggest Countries by Population
+SELECT * FROM Countries
+
+SELECT TOP(30) [CountryName]
+,[Population]
+FROM Countries
+WHERE ContinentCode = 'EU'
+ORDER BY [Population] DESC, CountryName
+
+--24. Countries and Currency (Euro / Not Euro)
+USE Geography
+SELECT CountryName
+,CountryCode
+,CurrencyCode
+FROM Countries
+ORDER BY CountryName
+
+SELECT* 
+FROM Countries
+ORDER BY CountryName
+
+
+
+
+
+--25. All Diablo Characters
+USE Diablo
+SELECT Name FROM Characters
+ORDER BY Name
